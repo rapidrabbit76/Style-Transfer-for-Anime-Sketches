c524271ca1ea1db97869d0898fe426b7cc10a833
yslee <yslee.dev@gmail.com>
2022-03-13 02:07:36 +0900

    feat: disc

---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from models.vgg import VGG19
+from models.guide_decoder import GuideDecoder
+from models.generator import UnetGenerator
+from models.discriminator import Discriminator
---
+++ models/discriminator.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+from models.blocks import ConvBlock
+
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels: int):
+        super(Discriminator, self).__init__()
+        dim = 32
+        self.blocks = nn.Sequential(
+            nn.Conv2d(in_channels, dim, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2),
+            ConvBlock(dim * 1, dim * 2, 4, 2, 1),
+            ConvBlock(dim * 2, dim * 4, 4, 2, 1),
+            ConvBlock(dim * 4, dim * 8, 4, 2, 1),
+            nn.Conv2d(dim * 8, dim * 8, 4, 2, 1, bias=False),
+        )
+        self.head = nn.Sequential(nn.Flatten(), nn.Linear(16384, 1))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.blocks(x)
+        x = self.head(x)
+        return x
